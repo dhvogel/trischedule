6139a4373c4e81904d2afcb1e54b8f7a5850e2f7
Dan Vogel <dhvogel2468@gmail.com>
2019-10-23 21:28:08 -0700

    clean up

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,5 +2,3 @@ VERSION=0.1.0
 
 gcloud --project trischedule builds submit --tag gcr.io/trischedule/trischedule:$VERSION
 gcloud --project trischedule beta run deploy trischedule --image gcr.io/trischedule/trischedule:$VERSION --platform managed --region us-central1 --memory 512M
-
-
