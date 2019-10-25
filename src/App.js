47bf76e26a02a4fae18c07826e6b5fbade36da40
Dan Vogel <dhvogel2468@gmail.com>
2019-10-24 20:06:38 -0700

    split out schedule into own component

--- src/App.js
+++ src/App.js
@@ -1,70 +1,13 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Divider from '@material-ui/core/Divider';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
-import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import Typography from '@material-ui/core/Typography';
 import './App.css';
 import T10eHeader from './components/t10e-header';
+import T10eSchedule from './components/t10e-schedule';
 
 function App() {
   return (
     <div className="App">
       <T10eHeader />
-      <List style={{paddingTop: 30, paddingLeft: 20, paddingRight:50}}>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <DirectionsRunIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText secondary="Jan 9, 2014" />
-          <Divider/>
-        </ListItem>
-        <Divider/>
-        <List>
-          <ListItem>
-            <Card style={{width: '100%'}}>
-              <CardContent>
-                <Grid container spacing={3}>
-                  <Grid item xs={11}>
-                    <Typography>
-                      Run
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={1}>
-                    <CheckCircleOutlineIcon style={{color: 'gray'}}/>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          </ListItem>
-          <ListItem>
-            <Card style={{width: '100%'}}>
-              <CardContent>
-                <Grid container spacing={3}>
-                  <Grid item xs={11}>
-                    <Typography>
-                      Bike
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={1}>
-                    <CheckCircleIcon style={{color: 'green'}}/>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          </ListItem>
-        </List>
-      </List>
+      <T10eSchedule />
     </div>
   );
 }
---
+++ src/components/t10e-schedule/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import Typography from '@material-ui/core/Typography';
+
+class T10eSchedule extends React.Component<Props> {
+  render() {
+    return (
+      <List style={{paddingTop: 30, paddingLeft: 20, paddingRight:50}}>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              <DirectionsRunIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText secondary="Jan 9, 2014" />
+        </ListItem>
+        <Divider/>
+        <List>
+          <ListItem>
+            <Card style={{width: '100%'}}>
+              <CardContent>
+                <Grid container spacing={3}>
+                  <Grid item xs={11}>
+                    <Typography>
+                      Run
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <CheckCircleOutlineIcon style={{color: 'gray'}}/>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </ListItem>
+          <ListItem>
+            <Card style={{width: '100%'}}>
+              <CardContent>
+                <Grid container spacing={3}>
+                  <Grid item xs={11}>
+                    <Typography>
+                      Bike
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={1}>
+                    <CheckCircleIcon style={{color: 'green'}}/>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </ListItem>
+        </List>
+      </List>
+    )
+  }
+}
+
+export default T10eSchedule;
