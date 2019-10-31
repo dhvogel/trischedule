24f2c7d6596ea0b83226f50fc50bdd1747b1519b
Dan Vogel <dhvogel2468@gmail.com>
2019-10-30 23:19:55 -0700

    build out frame of main page

--- trischedule-web/src/components/t10e-schedule/index.js
+++ trischedule-web/src/components/t10e-schedule/index.js
@@ -12,6 +12,15 @@ import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import axios from 'axios';
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+  cardcontent: {
+    "&:last-child": {
+      paddingBottom: 14
+    }
+  }
+};
 
 class T10eSchedule extends React.Component<Props> {
   state = {
@@ -20,59 +29,73 @@ class T10eSchedule extends React.Component<Props> {
 
   componentDidMount() {
     const backendAddress = process.env.REACT_APP_TRISCHEDULE_BACKEND_ADDRESS
-    axios.get(`${backendAddress}/athlete/abc`)
+    axios.get(`${backendAddress}/athlete/abc/schedule`)
       .then(res => {
         console.log(res)
-        // const posts = res.data.data.children.map(obj => obj.data);
-        // this.setState({ posts });
+        const workouts = res.data.workouts;
+        this.setState({ workouts });
     });
   }
 
   render() {
+    if (!this.state.workouts) return null
+    const {classes} = this.props
+    const workouts = this.state.workouts
+    const workoutsGroupedByDate = workouts.reduce((workoutsSoFar, {scheduled_day, workout_type, completed, workout_comments}) => {
+      if (!workoutsSoFar[scheduled_day]) workoutsSoFar[scheduled_day] = []
+      workoutsSoFar[scheduled_day].push({workout_type, completed, workout_comments});
+      return workoutsSoFar
+    }, {})
     return (
-      <List style={{paddingTop: 30, paddingLeft: 20, paddingRight:50}}>
-        <ListItem>
-          <ListItemAvatar>
-            <Avatar>
-              <DirectionsRunIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText secondary="Jan 9, 2014" />
-        </ListItem>
-        <Divider/>
-        <List>
-          <ListItem>
-            <Card style={{width: '100%'}}>
-              <CardContent>
-                <Grid container spacing={3}>
-                  <Grid item xs={11}>
-                      Run
-                  </Grid>
-                  <Grid item xs={1}>
-                    <CheckCircleOutlineIcon style={{color: 'gray'}}/>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          </ListItem>
-          <ListItem>
-            <Card style={{width: '100%'}}>
-              <CardContent>
-                <Grid container spacing={3}>
-                  <Grid item xs={11}>
-                      Bike
-                  </Grid>
-                  <Grid item xs={1}>
-                    <CheckCircleIcon style={{color: 'green'}}/>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
-          </ListItem>
+      <div>
+        <List style={{paddingTop: 60, paddingLeft: 20, paddingRight:50}}>
+          {
+            Object.entries(workoutsGroupedByDate).map(([date, workouts], idx) => {
+              return (
+              <div>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <DirectionsRunIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText secondary={date} />
+              </ListItem>
+              <Divider/>
+              <List>
+                {
+                  workouts.map((workout) =>
+                    <ListItem>
+                      <Card style={{width: '100%'}}>
+                        <CardContent
+                          className={classes.cardcontent}
+                        >
+                          <Grid container spacing={3}>
+                            <Grid item xs={11}>
+                                {workout.workout_type}
+                            </Grid>
+                            <Grid item xs={1}>
+                              {workout.completed ? (
+                                  <CheckCircleIcon style={{color: 'green'}}/>
+                                ) : (
+                                  <CheckCircleOutlineIcon style={{color: 'gray'}}/>
+                                )
+                              }
+                            </Grid>
+                          </Grid>
+                        </CardContent>
+                      </Card>
+                    </ListItem>
+                  )
+                }
+              </List>
+            </div>)
+            })
+          }
         </List>
-      </List>
+      </div>
     )
   }
 }
 
-export default T10eSchedule;
+export default withStyles(styles)(T10eSchedule);
