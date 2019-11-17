cdc6b43835b3fa1104aa84264b5423ad3d9101d0
Dan Vogel <dhvogel2468@gmail.com>
2019-11-17 13:30:02 -0800

    add build and deploy script for backend

---
+++ trischedule-backend/build_and_deploy.sh
@@ -0,0 +1,4 @@
+VERSION=0.1.2
+
+gcloud --project trischedule builds submit --tag gcr.io/trischedule/trischedule-backend:$VERSION
+gcloud --project trischedule beta run deploy trischedule-backend --image gcr.io/trischedule/trischedule-backend:$VERSION --platform managed --region us-central1 --memory 512 --allow-unauthenticated
