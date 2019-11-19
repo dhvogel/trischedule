af40d7833eecf01857a5e6f0a240a7250a32396a
Dan Vogel <dhvogel2468@gmail.com>
2019-11-18 20:15:19 -0800

    revised build and deploy script, back to cloud run

--- trischedule-web/build_and_deploy.sh
+++ trischedule-web/build_and_deploy.sh
@@ -1,4 +1,6 @@
-npm run build --production
+VERSION=0.1.4
 
-gcloud --project trischedule app deploy
+docker build -t gcr.io/trischedule/trischedule-web:$VERSION .
+docker push gcr.io/trischedule/trischedule-web:$VERSION
+gcloud beta run deploy --image gcr.io/trischedule/trischedule-web:$VERSION --platform managed
 
